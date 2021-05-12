b23ed5e1e252caea4468eb93bb9d00d41d1b5f0f
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-05-12 15:41:25 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
-const hamburguerNav = document.querySelector(".main-nav-container");
+const hamburguerNav = document.querySelector(".main-nav__hamburger-icon");
+const accordionItems = document.querySelectorAll(".main-nav__accordion-item-title")
 
-
-loadEvents()
+loadEvents();
 function loadEvents() {
-    hamburguerNav.addEventListener("click", showMenu)
+  // Show or hidde menu
+  hamburguerNav.addEventListener("click", showMenu);
+
+  //Accordion
+  accordionEffect();
 }
 
-function showMenu(e) {
-    if(e.target.classList){
-        console.log("activooooooooo");
-    }
-    
+// Show or hidde menu
+function showMenu() {
+    if (!hamburguerNav.nextElementSibling.classList.contains("main-nav--show")) {
+      hamburguerNav.src = "./images/icon-close.svg";
+      hamburguerNav.nextElementSibling.classList.add("main-nav--show");
+    } else {
+      hamburguerNav.src = "./images/icon-hamburger.svg";
+      hamburguerNav.nextElementSibling.classList.remove("main-nav--show");
+    }  
+}
+
+// Accordion
+function accordionEffect() {
+    accordionItems.forEach(item => {
+         console.log(item);
+         item.addEventListener("click", () =>{
+             console.log(item.nextElementSibling);
+         })
+    })    
 }
