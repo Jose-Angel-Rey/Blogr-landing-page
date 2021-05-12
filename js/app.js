320e806816736fb778fc91f66e5a7e71d86bf352
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-05-12 15:57:49 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const hamburguerNav = document.querySelector(".main-nav__hamburger-icon");
-const accordionItems = document.querySelectorAll(".main-nav__accordion-item-title")
+const accordionItems = document.querySelectorAll(
+  ".main-nav__accordion-item-title"
+);
 
 loadEvents();
 function loadEvents() {
@@ -12,21 +14,57 @@ function loadEvents() {
 
 // Show or hidde menu
 function showMenu() {
-    if (!hamburguerNav.nextElementSibling.classList.contains("main-nav--show")) {
-      hamburguerNav.src = "./images/icon-close.svg";
-      hamburguerNav.nextElementSibling.classList.add("main-nav--show");
-    } else {
-      hamburguerNav.src = "./images/icon-hamburger.svg";
-      hamburguerNav.nextElementSibling.classList.remove("main-nav--show");
-    }  
+  if (!hamburguerNav.nextElementSibling.classList.contains("main-nav--show")) {
+    hamburguerNav.src = "./images/icon-close.svg";
+    hamburguerNav.nextElementSibling.classList.add("main-nav--show");
+  } else {
+    hamburguerNav.src = "./images/icon-hamburger.svg";
+    hamburguerNav.nextElementSibling.classList.remove("main-nav--show");
+  }
 }
 
 // Accordion
 function accordionEffect() {
-    accordionItems.forEach(item => {
-         console.log(item);
-         item.addEventListener("click", () =>{
-             console.log(item.nextElementSibling);
-         })
-    })    
+  accordionItems.forEach((item) => {
+    console.log(item);
+    item.addEventListener("click", () => {
+      if (
+        !item.nextElementSibling.classList.contains(
+          "main-nav__accordion-item-content--show"
+        )
+      ) {
+        const checkItems = document.querySelectorAll(
+          ".main-nav__accordion-item-title"
+        );
+        checkItems.forEach((otherItem) => {
+          if (
+            otherItem.nextElementSibling.classList.contains(
+              "main-nav__accordion-item-content--show"
+            )
+          ) {
+            otherItem.classList.remove(
+              "main-nav__accordion-item-title--active"
+            );
+            otherItem.nextElementSibling.classList.remove(
+              "main-nav__accordion-item-content--show"
+            );
+          }
+        });
+
+        item.classList.add("main-nav__accordion-item-title--active");
+        item.nextElementSibling.classList.add(
+          "main-nav__accordion-item-content--show"
+        );
+      } else if (
+        item.nextElementSibling.classList.contains(
+          "main-nav__accordion-item-content--show"
+        )
+      ) {
+        item.classList.remove("main-nav__accordion-item-title--active");
+        item.nextElementSibling.classList.remove(
+          "main-nav__accordion-item-content--show"
+        );
+      }
+    });
+  });
 }
