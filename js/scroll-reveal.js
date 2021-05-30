ee4b8249697c27c0b0677431e3471495b506c02f
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-05-30 06:47:27 -0400

    Update

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -2,19 +2,6 @@
 //      ScrollReveal animations
 // *******************************
 
-ScrollReveal().reveal(".main-nav-container", {
-  duration: 2000,
-  delay: 500,
-  origin: "top",
-  distance: "2rem",
-});
-ScrollReveal().reveal(".main-header__content", {
-  duration: 2000,
-  delay: 500,
-  origin: "bottom",
-  distance: "2rem",
-});
-
 // Main content - Section one
 ScrollReveal().reveal(".section-one__title", {
   duration: 2000,
